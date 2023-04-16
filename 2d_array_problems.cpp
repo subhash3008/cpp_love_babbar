d0894ce16bdff525b48f1838c9fb0cea77c14c34
s.chandra <subhash.chandra@intive.com>
2023-04-16 17:07:36 +0200

    more questions

---
+++ 2d_array_problems.cpp
@@ -0,0 +1,246 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_vector(vector<vector<int>> vect)
+{
+  for (int row = 0; row < vect.size(); ++row)
+  {
+    vector<int> v_row = vect.at(row);
+    for (int col = 0; col < v_row.size(); ++col)
+    {
+      cout << v_row.at(col) << " ";
+    }
+    cout << endl;
+  }
+}
+
+bool is_present(int arr[][3], int target, int rowCount, int colCount)
+{
+  for (int row = 0; row < rowCount; ++row)
+  {
+    for (int col = 0; col < colCount; ++col)
+    {
+      if (target == arr[row][col])
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void print_row_sum(int arr[][3], int rowCount, int colCount)
+{
+  int sum = 0;
+  for (int row = 0; row < rowCount; ++row)
+  {
+    for (int col = 0; col < colCount; ++col)
+    {
+      sum += arr[row][col];
+    }
+    cout << "Sum for row " << row << " is " << sum << "." << endl;
+    sum = 0;
+  }
+}
+
+int larget_sum_row(int arr[][3], int rowCount, int colCount)
+{
+  int arr_sum[rowCount] = { 0 };
+  int sum = 0;
+  for (int row = 0; row < rowCount; ++row)
+  {
+    for (int col = 0; col < colCount; ++col)
+    {
+      sum += arr[row][col];
+    }
+    arr_sum[row] = sum;
+    sum = 0;
+  }
+
+  int max = INT_MIN;
+  int maxIndex = 0;
+  for (int i = 0; i < rowCount; ++i)
+  {
+    if (max < arr_sum[i])
+    {
+      max = arr_sum[i];
+      maxIndex = i;
+    }
+  }
+
+  return maxIndex;
+}
+
+void print_like_wave(vector<vector<int>> vect, int nRows, int mCols)
+{
+  for (int col = 0; col < mCols; ++col)
+  {
+    if (col & 1)
+    {
+      // odd -> bottom to top
+      for (int row = nRows - 1; row >= 0; --row)
+      {
+        cout << vect[row][col] << " ";
+      }
+    }
+    else
+    {
+      // even -> top to bottom
+      for (int row = 0; row < nRows; ++row)
+      {
+        cout << vect[row][col] << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+vector<int> print_spiral(vector<vector<int>> vect)
+{
+  vector<int> ans;
+
+  int nRows = vect.size();
+  int mCols = vect[0].size();
+
+  int count = 0;
+  int total = nRows * mCols;
+
+  int startingRow = 0;
+  int startingCol = 0;
+  int endingRow = nRows - 1;
+  int endingCol = mCols - 1;
+
+  while (count < total)
+  {
+    // starting row
+    for (int idx = startingCol; count < total && idx <= endingCol; ++idx)
+    {
+      ans.push_back(vect[startingRow][idx]);
+      count++;
+    }
+    startingRow++;
+
+    // ending col
+    for (int idx = startingRow; count < total && idx <= endingRow; ++idx)
+    {
+      ans.push_back(vect[idx][endingCol]); 
+      count++;
+    }
+    endingCol--;
+
+    // ending row
+    for (int idx = endingCol; count < total && idx >= startingCol; --idx)
+    {
+      ans.push_back(vect[endingRow][idx]); 
+      count++;
+    }
+    endingRow--;
+
+    // starting col
+    for (int idx = endingRow; count < total && idx >= startingRow; --idx)
+    {
+      ans.push_back(vect[idx][startingCol]); 
+      count++;
+    }
+    startingCol++;
+  }
+  return ans;
+}
+
+vector<vector<int>> rotate_by_90_degree(vector<vector<int>> vect)
+{
+  vector<vector<int>> res;
+  int nRows = vect.size();
+  int mCols = vect[0].size();
+
+  for (int col = 0; col < mCols; ++col)
+  {
+    vector<int> v_row;
+    for (int row = nRows - 1; row >= 0; --row)
+    {
+      v_row.push_back(vect[row][col]);
+    }
+    res.push_back(v_row);
+  }
+  return res;
+}
+
+int main()
+{
+  // int arr[3][4] = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}};
+  int arr[3][3];
+
+  // Take input
+  // cout << "Enter the elements : " << endl;
+  // for (int row = 0; row < 3; ++row)
+  // {
+  //   for (int col = 0; col < 3; ++col)
+  //   {
+  //     cin >> arr[row][col];
+  //   }
+  // }
+
+  // display elements
+  // cout << "Printing the elements.." << endl;
+  // for (int row = 0; row < 3; ++row)
+  // {
+  //   for (int col = 0; col < 3; ++col)
+  //   {
+  //     cout << arr[row][col] << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // Search
+  // cout << "Enter the element to search : ";
+  // int target;
+  // cin >> target;
+
+  // if (is_present(arr, target, 3, 4))
+  // {
+  //   cout << "Element found";
+  // }
+  // else
+  // {
+  //   cout << "Element not found";
+  // }
+
+  // print_row_sum(arr, 3, 3);
+
+  // int row_num = larget_sum_row(arr, 3, 3);
+  // cout << "Largest sum exist in row " << row_num << endl;
+
+  vector<vector<int>> vect;
+  int r = 3;
+  int c = 3;
+  cout << "enter the elements : ";
+  for (int row = 0; row < r; ++row)
+  {
+    vector<int> v_row;
+    for (int col = 0; col < c; ++col)
+    {
+      int temp;
+      cin >> temp;
+      v_row.push_back(temp);
+    }
+    vect.push_back(v_row);
+  }
+  cout << "Printing..." << endl;
+  print_vector(vect);
+
+  // print_like_wave(vect, r, c);
+
+  // vector<int> res = print_spiral(vect);
+  // for (int i = 0; i < res.size(); ++i)
+  // {
+  //   cout << res.at(i) << " ";
+  // }
+  // cout << endl;
+
+  cout << "Rotating..." << endl;
+  print_vector(rotate_by_90_degree(vect));
+
+  return 0;
+}
--- strings.cpp
+++ strings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -127,10 +128,188 @@ string reverse_words_in_str(string str)
   return res_string;
 }
 
+char get_max_occuring_char(string str)
+{
+  int arr[26];
+  for (int i = 0; i < 26 ; ++i)
+  {
+    arr[i] = 0;
+  }
+
+  int len_str = str.length();
+  for (int i = 0; i < len_str; ++i)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      arr[str[i] - 'a']++;
+    }
+    else if (str[i] >= 'A' && str[i] <= 'Z')
+    {
+      arr[str[i] - 'A']++;
+    }
+  }
+
+  int max = arr[0];
+  int max_idx = 0;
+  for (int i = 1; i < 26; ++i)
+  {
+    if (max < arr[i])
+    {
+      max = arr[i];
+      max_idx = i;
+    }
+  }
+  return 'a' + max_idx; 
+}
+
+string replace_spaces(string &str)
+{
+  string temp = "";
+  
+  for (int i = 0; i < str.length(); ++i)
+  {
+    if (str[i] == ' ')
+    {
+      temp.push_back('@');
+      temp.push_back('4');
+      temp.push_back('0');
+    }
+    else
+    {
+      temp.push_back(str[i]);
+    }
+  }
+  return temp;
+}
+
+string remove_all_occurence_of_substring(string str, const string part)
+{
+  while (str.length() && (str.find(part) < str.length()))
+  {
+    str.erase(str.find(part), part.length());
+  }
+  return str;
+}
+
+// Checks if the permutation of the part exists in
+// the provided input string.. e.g. permutation of 'ab'
+// are 'ab' and 'ba'.. constraint: all letters in the
+// string and part
+bool check_count_equal(int arr[26], int arr2[26])
+{
+  for (int i = 0; i < 26; ++i)
+  {
+    if (arr[i] != arr2[i])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool does_permutation_exist(string part, string str)
+{
+  int count_part[26] = { 0 };
+  // store count of each character
+  for (int i = 0; i < part.length(); ++i)
+  {
+    count_part[part[i] - 'a']++;
+  }
+
+  // now traverse str in window of size part and compare
+  int i = 0;
+  int count_str[26] = { 0 };
+  int window_size = part.length();
+
+  while ((i < window_size) && (i < str.length()))
+  {
+    count_str[str[i] - 'a']++;
+    i++;
+  }
+
+  if (check_count_equal(count_part, count_str))
+  {
+    return true;
+  }
+
+  while (i < str.length())
+  {
+    count_str[str[i] - 'a']++;
+    count_str[str[i - window_size] - 'a']--;
+    i++;
+
+    if (check_count_equal(count_part, count_str))
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+string remove_adjacent_duplicates(string str)
+{
+  int count_removal = 0;
+  do
+  {
+    int i = 0;
+    count_removal = 0;
+    while (i < str.length())
+    {
+      if (str[i + 1] && (str[i] == str[i + 1]))
+      {
+        str.erase(i, 2);
+        count_removal++;
+        cout << "Iteration : " << i << " string : " << str << endl;
+        continue;
+      }
+      ++i;
+    }
+  } while (count_removal);
+  return str;
+}
+
+// returns length of compressed string
+int compress_string(vector<char>& chars)
+{
+  cout << "Input length " << chars.size() << endl; 
+  int i = 0;
+  int ansIndex = 0;
+  int n = chars.size();
+
+  while (i < n)
+  {
+    int j = i + 1;
+    while (j < n && (chars[i] == chars[j]))
+    {
+      j++;
+    }
+    
+    // store the initial character
+    chars[ansIndex++] = chars[i];
+    int count = j - i;
+    if (count > 1)
+    {
+      // convert count into single digit and put it in the vector
+      string cnt = to_string(count);
+      for (char ch: cnt)
+      {
+        chars[ansIndex++] = ch;
+      }
+    }
+    i = j;
+  }
+  cout << "vector : ";
+  for (int i = 0; i < ansIndex; ++i)
+  {
+    cout << chars[i];
+  }
+  cout << endl;
+  return ansIndex;
+}
+
 int main()
 {
-  
-  string str = "Hello Who are you";
   // char str[100];
   // cout << "Enter the string : ";
   // cin >> str;
@@ -151,9 +330,50 @@ int main()
   // cout << (is_valid_palindrome ? "Valid Palindrome" : "Not a valid palindrome") << endl;
 
   // REVERSE WORDS IN A STRING
-  cout << "Str : " << str << endl;
-  string r_str = reverse_words_in_str(str);
-  cout << "Reversed : " << r_str << endl;
+  // string str = "Hello Who are you";
+  // cout << "Str : " << str << endl;
+  // string r_str = reverse_words_in_str(str);
+  // cout << "Reversed : " << r_str << endl;
+
+  // MAXIMUM OCCURENCE
+  // string str;
+  // cin >> str;
+  // char ch = get_max_occuring_char(str);
+  // cout << "Max occuring char : " << ch << endl;
+
+  // REPLACE SPACES
+  // string str;
+  // getline(cin, str);
+  // string newstr = replace_spaces(str);
+  // cout << "New String : " << newstr << endl;
+
+  // REMOVE ALL OCCURENCES OF A SUBSTRING
+  // string str, part;
+  // cout << "Enter the sentence : ";
+  // getline(cin, str);
+  // cout << "Enter substring : ";
+  // getline(cin, part);
+  // string res = remove_all_occurence_of_substring(str, part);
+  // cout << "Resulting string : " << res;
+
+  // PERMUTATION IN STRING
+  // string str, part;
+  // cout << "Enter the sentence : ";
+  // getline(cin, str);
+  // cout << "Enter substring : ";
+  // getline(cin, part);
+  // cout << "Does permutation exist : " << (does_permutation_exist(part, str) ? "Yes" : "No");
+
+  // REMOVE ADJACENT DUPLICATE CHARACTERS
+  // string str;
+  // getline(cin, str);
+  // string res = remove_adjacent_duplicates(str);
+  // cout << "Res : " << res;
+
+  // STRING COMPRESSION.. Leetcode 443 for description
+  vector<char> vec = {'a', 'a', 'b', 'c', 'c', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'e', 'e', 'e'};
+  cout << "Res: " << compress_string(vec);
+
 
   return 0;
 }
